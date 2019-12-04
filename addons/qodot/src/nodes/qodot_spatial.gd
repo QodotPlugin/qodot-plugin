dd712982f870773425c9a915cc78e3d92125f1f8
Josh Palmer <jpalmerwatkins@gmail.com>
2019-12-04 22:00:21 +0000

    Remove unneeded add_child_editor methods

--- addons/qodot/src/nodes/qodot_spatial.gd
+++ addons/qodot/src/nodes/qodot_spatial.gd
@@ -3,15 +3,3 @@ extends Spatial
 
 func get_class():
 	return 'QodotSpatial'
-
-func add_child_editor(child: Node) -> Node:
-	add_child(child)
-
-	if(is_inside_tree()):
-		var tree = get_tree()
-		if(tree != null):
-			var edited_scene_root = tree.get_edited_scene_root()
-			if(edited_scene_root != null):
-				child.set_owner(edited_scene_root)
-
-	return child
--- addons/qodot/src/qodot_util.gd
+++ addons/qodot/src/qodot_util.gd
@@ -8,18 +8,3 @@ const DEBUG = false
 static func debug_print(msg):
 	if(DEBUG):
 		print(msg)
-
-# Adds the provided child to the provided object,
-# set its owner to the edited scene root to make it display in the editor tree,
-# then returns the child
-static func add_child_editor(obj: Node, child: Node) -> Node:
-	obj.add_child(child)
-
-	if(obj.is_inside_tree()):
-		var tree = obj.get_tree()
-		if(tree != null):
-			var edited_scene_root = tree.get_edited_scene_root()
-			if(edited_scene_root != null):
-				child.set_owner(edited_scene_root)
-
-	return child
