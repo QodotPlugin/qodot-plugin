caf1a599f27711272ff60808ab52fc951f5e32fe
Josh Palmer <jpalmerwatkins@gmail.com>
2019-12-11 00:42:04 +0000

    Fix palette / WAD import plugin naming, instancing

--- addons/qodot/src/import_plugins/quake_wad_import_plugin.gd
+++ addons/qodot/src/import_plugins/quake_wad_import_plugin.gd
@@ -1,4 +1,4 @@
-class_name QuakePaletteImportPlugin
+class_name QuakeWadImportPlugin
 extends EditorImportPlugin
 tool
 
--- addons/qodot/src/qodot_plugin.gd
+++ addons/qodot/src/qodot_plugin.gd
@@ -6,17 +6,22 @@ tool
 
 var map_import_plugin = null
 var palette_import_plugin = null
+var wad_import_plugin = null
 
 func _enter_tree():
 	map_import_plugin = QuakeMapImportPlugin.new()
 	palette_import_plugin = QuakePaletteImportPlugin.new()
+	wad_import_plugin = QuakeWadImportPlugin.new()
 
 	add_import_plugin(map_import_plugin)
 	add_import_plugin(palette_import_plugin)
+	add_import_plugin(wad_import_plugin)
 
 func _exit_tree():
 	remove_import_plugin(map_import_plugin)
 	remove_import_plugin(palette_import_plugin)
+	remove_import_plugin(wad_import_plugin)
 
 	map_import_plugin = null
 	palette_import_plugin = null
+	wad_import_plugin = null
