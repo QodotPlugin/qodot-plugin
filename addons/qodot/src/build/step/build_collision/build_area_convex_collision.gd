7e0f1ef54b8fa1bcf66f68b186f11c28d6d1daf7
Josh Palmer <jpalmerwatkins@gmail.com>
2020-02-09 01:00:35 +0000

    Build one collision area per trigger

--- addons/qodot/src/build/step/build_collision/build_area_convex_collision.gd
+++ addons/qodot/src/build/step/build_collision/build_area_convex_collision.gd
@@ -24,22 +24,33 @@ func _finalize(context) -> Dictionary:
 
 	var area_collision_dict = {}
 
+
 	for entity_key in area_collision_shapes:
+		var area_collision_shape_key = entity_key + '_trigger'
+		var brush_area = Area.new()
+		brush_area.name = area_collision_shape_key
+		var entity_center = Vector3.ZERO
+
 		for brush_key in area_collision_shapes[entity_key]:
 			var area_collision_shape_data = area_collision_shapes[entity_key][brush_key]
-			var area_collision_shape_key = entity_key + '_' + brush_key + '_trigger'
+			var brush_center = area_collision_shape_data['brush_center']
+			entity_center += brush_center
+		entity_center /= area_collision_shapes.size()
+
+		for brush_key in area_collision_shapes[entity_key]:
+			var area_collision_shape_data = area_collision_shapes[entity_key][brush_key]
 
 			var brush_center = area_collision_shape_data['brush_center']
 			var brush_collision_vertices = area_collision_shape_data['brush_collision_vertices']
 
 			var brush_collision_shape = create_convex_collision_shape(brush_collision_vertices)
+			brush_collision_shape.name = brush_key
+			brush_collision_shape.translation = brush_center - entity_center
 
-			var brush_area = Area.new()
-			brush_area.name = area_collision_shape_key
-			brush_area.translation = brush_center
 			brush_area.add_child(brush_collision_shape)
 
-			area_collision_dict[area_collision_shape_key] = brush_area
+		brush_area.translation = entity_center
+		area_collision_dict[area_collision_shape_key] = brush_area
 
 	return {
 		'nodes': {
