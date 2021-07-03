8d0ca3fbfe7711f4bc262cc6fae68ef28074e619
David Stout <david.stout@deadhostgames.com>
2021-07-03 09:32:35 +0100

    Change _generate_base_class_list to use printerr

--- addons/qodot/src/resources/game-definitions/fgd/qodot_fgd_file.gd
+++ addons/qodot/src/resources/game-definitions/fgd/qodot_fgd_file.gd
@@ -123,6 +123,6 @@ func _generate_base_class_list(entity_def : Resource, visited_base_classes = [])
 			base_classes.append(base_class)
 			base_classes += _generate_base_class_list(base_class, visited_base_classes)
 		else:
-			push_error(str("Entity '", entity_def.classname,"' contains cycle/duplicate to Entity '", base_class.classname, "'"))
+			printerr(str("Entity '", entity_def.classname,"' contains cycle/duplicate to Entity '", base_class.classname, "'"))
 
 	return base_classes
