df2eb6bac092f2cd0d8da51fe88a4f3febd6ce30
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-01-03 23:33:31 -0800

    Move code to the root.

--- build-x11.sh
+++
@@ -1,4 +0,0 @@
-scons -c platform=x11 bits=64 target=debug
-scons platform=x11 bits=64 target=debug
-cp libqodot/build/libqodot.so addons/qodot/bin/x11/
-cp libqodot/libmap/build/libmap.so addons/qodot/bin/x11/
